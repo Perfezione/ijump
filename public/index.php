92d965585b279dfdd6478ffa11b2fc81962dc284
Lennart De Vink <lennartdevink@MacBook-Pro-van-Lennart.local>
2016-05-20 12:28:01 +0200

    fixed getUser

--- public/index.php
+++ public/index.php
@@ -1,61 +1,20 @@
-<?php
-require_once("../classes/user.php");
-
-if (PHP_SAPI == 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
-    $file = __DIR__ . $_SERVER['REQUEST_URI'];
-    if (is_file($file)) {
-        return false;
-    }
-}
-
-/* Connect to an ODBC database using an alias */
-$dsn = 'ijump';
-$user = 'root';
-$password = '';
-
-try {
-    $dbh = new PDO($dsn, $user, $password);
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
-
-
-require __DIR__ . '/../vendor/autoload.php';
-
-session_start();
-
-$usermodel = new UserModel();
-$sessionModel = new SesssionModel();
-
-// Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
-$app = new \Slim\App($settings);
-$app->get('/user', array($usermodel, 'getUser'));
-$app->post('/user', array($usermodel, 'createUser'));
-
-$app->get('/session', array($sessionmodel, 'getSession'));
-$app->post('/session', array($sessionmodel, 'createSession'));
-        
-class UserModel
+<?php        
+class UserModel extends Model
 {
-    private $dbconn;
-            
-    public function __construct(PDO $connection)
+    public function getUser(Slim\Http\Request $request)
     {
-        $this->dbConn = $connection;
-    }
-    
-    public function getUser()
-    {
-        
+        $userId = $request->getParam('id');
         $statement = $this->dbconn->prepare("SELECT * FROM user 
                                         WHERE id = :user_id");
         
         $statement->execute(array('user_id' => $userId));
         $row = $statement->fetch();
-        echo json_encode($row);
+        
+        if($row) {
+            echo json_encode($row);
+        } else {
+            echo "{}";
+        }
     }
     
    public function createUser() {
@@ -68,17 +27,24 @@ class UserModel
         $this->dbconn->query("Insert into user (phonenumber, name, password, weight)"
                 . "VALUES ('" . $phonenumber . "', '" . $name . "', '" . $password . "', '" . $weight . "')");
     } 
+    
+    
 }
 
-class SessionModel 
+abstract class Model
 {
-    private $dbconn;
+   
+    protected $dbconn;
 
     public function __construct(PDO $connection)
     {
-        $this->dbConn = $connection;
+        $this->dbconn = $connection;
     }
 
+}
+
+class SessionModel extends Model
+{
    public function getSession()
     {
         
@@ -105,8 +71,44 @@ class SessionModel
 }
 
 
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $file = __DIR__ . $_SERVER['REQUEST_URI'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+/* Connect to an ODBC database using an alias */
+$dsn = 'mysql:dbname=ijump;host=127.0.0.1';
+$user = 'root';
+$password = 'root';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+$usermodel = new UserModel($dbh);
+$sessionModel = new SessionModel($dbh);
+
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+$app = new \Slim\App($settings);
+$app->get('/user', array($usermodel, 'getUser'));
+$app->post('/user', array($usermodel, 'createUser'));
+
+$app->get('/session', array($sessionModel, 'getSession'));
+$app->post('/session', array($sessionModel, 'createSession'));
  
         
-        
+$app->run();
 
 
